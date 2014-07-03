a079375063d91e69fa112ae0355394bed79cfe05
Maxthon Chan <xcvista@me.com>
2014-07-03 14:05:48 +0800

    Initial Commit

---
+++ CGIJSONObjects/CGIJSONObjects.h
@@ -0,0 +1,19 @@
+//
+//  CGIJSONObjects.h
+//  CGIJSONObjects
+//
+//  Created by Maxthon Chan on 7/3/14.
+//  Copyright (c) 2014 Maxthon Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CGIJSONObjects.
+FOUNDATION_EXPORT double CGIJSONObjectsVersionNumber;
+
+//! Project version string for CGIJSONObjects.
+FOUNDATION_EXPORT const unsigned char CGIJSONObjectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGIJSONObjects/PublicHeader.h>
+
+
